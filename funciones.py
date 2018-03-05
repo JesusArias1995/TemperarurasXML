3c6bf0b62ad5c649b83afa7308e69f8f6845056f
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-03-05 09:35:38 +0100

    Modificado fichero

---
+++ funciones.py
@@ -0,0 +1,23 @@
+from lxml import etree
+import time
+
+doc = etree.parse('sevilla.xml')
+
+codigos=doc.findall("municipio")
+
+nombre=input("Dime el nombre de un municipio: ")
+
+for i in codigos:
+	if nombre.upper()==i.text.upper():
+		codigo=i.attrib["value"][-5:]
+
+url="http://www.aemet.es/xml/municipios/localidad_"+codigo+".xml"
+
+arbol=etree.parse(url)
+
+for i in arbol.findall("prediccion/dia"):
+	if i.attrib["fecha"]==time.strftime("%Y-%m-%d"):
+		maxima=i.find("temperatura/maxima")
+		minima=i.find("temperatura/minima")
+print("Maxima:", maxima.text)
+print("Minima:", minima.text)
